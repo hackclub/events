d27dc72d23dc75a10ffd0af72e93af89dd14b1c3
Lachlan Campbell <lachlan@hackclub.com>
2020-04-15 16:05:02 -0400

    Add meta tags

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import * as React from 'react'
 import NextApp from 'next/app'
+import Head from 'next/head'
 
 import '@hackclub/theme/fonts/reg-bold.css'
 import theme from '@hackclub/theme'
@@ -11,6 +12,15 @@ export default class App extends NextApp {
     const { Component, pageProps } = this.props
     return (
       <ThemeProvider theme={theme}>
+        <Head>
+          <title>Hack Club Events</title>
+          <Meta
+            name="Hack Club"
+            title="Events"
+            description="See the upcoming events from the Hack Club community: AMAs, Code in the Dark, live coding sessions, & lots more."
+            image="https://events.hackclub.com/card.png"
+          />
+        </Head>
         <Nav />
         <Component {...pageProps} />
       </ThemeProvider>
