6fde27821ed360ec53eceb9d4b99170524bceff6
Lachlan Campbell <lachlan@hackclub.com>
2020-06-26 18:16:59 +0000

    Add Fathom

--- pages/_app.js
+++ pages/_app.js
@@ -1,28 +1,38 @@
-import * as React from 'react'
+import React, { useEffect } from 'react'
+import { useRouter } from 'next/router'
 import NextApp from 'next/app'
 import Head from 'next/head'
 
 import Meta from '@hackclub/meta'
 import '@hackclub/theme/fonts/reg-bold.css'
-import theme from '@hackclub/theme'
+import theme from '../lib/theme'
 import { ThemeProvider } from 'theme-ui'
-import Nav from '../components/nav'
+import * as Fathom from 'fathom-client'
 
-export default class App extends NextApp {
-  render() {
-    const { Component, pageProps } = this.props
-    return (
-      <ThemeProvider theme={theme}>
-        <Meta
-          as={Head}
-          name="Hack Club"
-          title="Events"
-          description="See the upcoming events from the Hack Club community: AMAs, Code in the Dark, live coding sessions, & lots more."
-          image="https://events.hackclub.com/card.png"
-        />
-        <Nav />
-        <Component {...pageProps} />
-      </ThemeProvider>
-    )
-  }
+const App = ({ Component, pageProps }) => {
+  const router = useRouter()
+
+  useEffect(() => {
+    Fathom.load('ESMSQSYP', { includedDomains: ['events.hackclub.com'] })
+    const onRouteChangeComplete = () => Fathom.trackPageview()
+    router.events.on('routeChangeComplete', onRouteChangeComplete)
+    return () => {
+      router.events.off('routeChangeComplete', onRouteChangeComplete)
+    }
+  }, [])
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Meta
+        as={Head}
+        name="Hack Club"
+        title="Events"
+        description="See the upcoming events from the Hack Club high school coding community: AMAs, CTFs, fireside chats, live coding sessions, & lots more."
+        image="https://events.hackclub.com/card.png"
+      />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
+
+export default App
