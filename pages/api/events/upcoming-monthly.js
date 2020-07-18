ced6c0849e1fcc5e74372e1746540a2a64cf25cb
Lachlan Campbell <lachlan@hackclub.com>
2020-07-18 19:14:41 +0000

    Add API

---
+++ pages/api/events/upcoming-monthly.js
@@ -0,0 +1,15 @@
+const { getEvents } = require('../../../lib/data')
+const { groupBy, filter } = require('lodash')
+
+export default async (req, res) => {
+  let events = await getEvents()
+  // Filter out events from previous months
+  events = filter(
+    events,
+    e =>
+      new Date(new Date(e.end.substring(0, 7)).toISOString().substring(0, 7)) >=
+      new Date(new Date().toISOString().substring(0, 7))
+  )
+  const months = groupBy(events, e => e.start.substring(0, 7))
+  res.json(months)
+}
