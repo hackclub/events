0fbb664ca06240ccddd2fe51fe9aeb099b9298c6
Theo B <me@theos.space>
2020-07-22 18:52:35 -0400

    Move events page over to new infrastructure

--- components/ama-rsvp.js
+++ components/ama-rsvp.js
@@ -9,90 +9,35 @@ import {
   Label,
   Text,
   Select,
-  Checkbox
+  Checkbox,
+  Link
 } from 'theme-ui'
 import useForm from '../hooks/use-form'
 import fetch from 'isomorphic-unfetch'
 import Router from 'next/router'
 
 const AMARsvp = ({ id, amaId }) => {
-  const { status, formProps, useField } = useForm(
-    '/api/ama-rsvp',
-    r => {
-      if (!r.waiver) {
-        Router.push('/waiver')
-      }
-    },
-    {
-      clearOnSubmit: 2000,
-      method: 'post',
-      extraData: {
-        id: amaId
-      }
-    }
-  )
-
   return (
     <Card>
       <Heading variant="headline" sx={{ mt: 0, mb: 1 }}>
         RSVP for this AMA
       </Heading>
-      <Grid
-        gap={3}
-        columns="1fr 1fr"
-        sx={{
-          mb: 3,
-          label: { display: 'flex', flexDirection: 'column' },
-          'input,select': { bg: 'sunken', boxShadow: 'none' }
-        }}
-        as="form"
-        {...formProps}
-      >
-        <Label>
-          Full Name
-          <Input
-            {...useField('name')}
-            placeholder="Hacker McHackerston"
-            required
-          />
-        </Label>
-        <Label>
-          Email
-          <Input
-            {...useField('email')}
-            placeholder="mchackerston@hacker.org"
-            required
-          />
-        </Label>
-        <Label sx={{ gridColumn: 'span 2' }}>
-          What’s your Slack handle?
-          <Input {...useField('slack')} placeholder="@mchacker" required />
-        </Label>
-        <Label sx={{ gridColumn: 'span 2' }}>
-          How are you primarily associated with Hack Club?
-          <Select {...useField('association')} variant="forms.input" required>
-            <option value="" disabled hidden>
-              Select one…
-            </option>
-            <option value="club-leader">I lead a club</option>
-            <option value="club-member">I am a club member</option>
-            <option value="slack-member">I am active on Slack</option>
-            <option value="alum">I am a Hack Club alum</option>
-            <option value="none">None of the above</option>
-          </Select>
-        </Label>
+      <Text variant="subtitle" mb={[3, 4]}>
+        Click the button below.
+      </Text>
+      <Link href={`https://ama-machine.hackclub.com/rsvp/${amaId}`}>
         <Button
           as="button"
           type="submit"
-          children={status === 'success' ? 'Signed up!' : 'RSVP me'}
           sx={{
             gridColumn: 'span 2',
             justifyContent: 'center',
-            bg: status === 'success' ? 'green' : 'primary',
             color: 'white'
           }}
-        />
-      </Grid>
+        >
+          RSVP via the Hack Club Slack
+        </Button>
+      </Link>
     </Card>
   )
 }
---
+++ pages/ama-success.js
@@ -0,0 +1,49 @@
+import { Card, Container, Text, Heading, Button } from 'theme-ui'
+import { useRouter, withRouter } from 'next/router'
+
+export default () => {
+  const {
+    query: { waiver, email }
+  } = useRouter()
+  return (
+    <Card
+      as="article"
+      sx={{ my: 4, textAlign: 'center', variant: 'layout.copy' }}
+    >
+      <Heading as="h1" variant="title" color="red">
+        Thanks for RSVPing!
+      </Heading>
+      <Text as="p" variant="subtitle" mt={2}>
+        We’re really excited to have you. Check your Slack DMs for a
+        confirmation.
+      </Text>
+      {waiver === '1' ? (
+        <>
+          <Text sx={{ fontSize: 2, my: 4 }}>
+            Also—since we livestream AMAs publicly, we’ll need you to fill out a
+            waiver. Make&nbsp;sure to use the same email address that's
+            associated with your Slack account ({email}).
+          </Text>
+
+          <Heading as="h2" variant="headline">
+            Sign your waiver
+          </Heading>
+
+          <Button
+            as="a"
+            target="_blank"
+            href="https://hack.af/ama-waiver-u18"
+            bg="accent"
+            mr={3}
+          >
+            I’m under 18
+          </Button>
+
+          <Button as="a" target="_blank" href="https://hack.af/ama-waiver-o18">
+            I’m over 18
+          </Button>
+        </>
+      ) : null}
+    </Card>
+  )
+}
