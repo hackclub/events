6fb59170f2e2e832239c89c79044cbe9e5ae4bb6
Lachlan Campbell <lachlan@hackclub.com>
2020-07-24 17:07:18 +0000

    Add social card

--- pages/_app.js
+++ pages/_app.js
@@ -32,7 +32,7 @@ const App = ({ Component, pageProps }) => {
         name="Hack Club"
         title="Events"
         description="See the upcoming events from the Hack Club high school coding community: AMAs, CTFs, fireside chats, live coding sessions, & lots more."
-        image="https://events.hackclub.com/card.png"
+        image="https://cloud-35gbvrq0s.vercel.app/2020-07-24_jqvwtt2xkqye541v21gqagg55zdevpj4.jpeg"
       />
       <Nav />
       <Component {...pageProps} />
