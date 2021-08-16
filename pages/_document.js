a17af859af561ab4a0d59a074828b6d7e29fde9c
Sam Poder <39828164+sampoder@users.noreply.github.com>
2021-08-16 11:50:03 +0800

    Revert "Update _document.js"
    This reverts commit e38d4513f26d3ee27f31207df3b5dbe66b4a0e56.

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
+import { InitializeColorMode } from 'theme-ui'
 
 export default class extends Document {
   static async getInitialProps(ctx) {
@@ -11,13 +12,9 @@ export default class extends Document {
       <Html lang="en">
         <Head />
         <body>
+          <InitializeColorMode />
           <Main />
           <NextScript />
-          <link rel="preconnect" href="https://fonts.gstatic.com" />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Fraunces:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
-            rel="stylesheet"
-          ></link>
         </body>
       </Html>
     )
