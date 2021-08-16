62d552b7fb226a6833df6cca9f5199d46a26802f
Sam Poder <39828164+sampoder@users.noreply.github.com>
2021-08-16 11:49:37 +0800

    Revert "Update _app.js"
    This reverts commit 8ec0eb24adc5f8aa3437cf6bf702844218967460.

--- pages/_app.js
+++ pages/_app.js
@@ -11,9 +11,9 @@ const App = ({ Component, pageProps }) => (
     <Meta
       as={Head}
       name="Hack Club"
-      title="Events in the Wild West"
+      title="Events"
       description="See the upcoming events from the Hack Club high school coding community: AMAs, CTFs, fireside chats, live coding sessions, & lots more."
-      image="https://events.hackclub.com/hc_events_wild_west.png"
+      image="https://cloud-35gbvrq0s.vercel.app/2020-07-24_jqvwtt2xkqye541v21gqagg55zdevpj4.jpeg"
     />
     <Nav />
     <Component {...pageProps} />
