8b1ea4471605773f1710a19741d9f13587ae0f19
Sam Poder <39828164+sampoder@users.noreply.github.com>
2021-08-16 11:49:33 +0800

    Revert "Update [slug].js"
    This reverts commit be1e87ccafe4069f68320f92eb62217af05a58d1.

--- pages/[slug].js
+++ pages/[slug].js
@@ -98,10 +98,10 @@ const Page = ({ event }) => (
               as="a"
               target="_blank"
               href={event.cal}
-              sx={{ bg: 'primary', mb: [3, 4], display: 'flex', width: 'max-content' }}
+              sx={{ bg: 'cyan', mb: [3, 4] }}
             >
               <Calendar />
-              <Text ml={2}>Add to Google Calendar</Text>
+              Add to Google Calendar
             </Button>
           )}
           {/* !event.ama && <RSVP {...event} /> */}
