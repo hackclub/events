dc8da17ce5f39f5c2e84917fc82e97e6b2085502
Belle <bellesee1212@gmail.com>
2022-04-16 22:43:16 +0800

    change data page description & image

--- pages/data.js
+++ pages/data.js
@@ -8,9 +8,9 @@ export default () => (
     <Meta
       as={Head}
       title="Data API"
-      name="Hack Club Hackathons"
-      description="API for listing upcoming high school hackathons around the world."
-      image="https://hackathons.hackclub.com/card.png"
+      name="Hack Club Events"
+      description="API for Hack Club Events."
+      image="https://events.hackclub.com/card.png"
     />
     <Container as={BaseStyles} variant="copy" sx={{ py: 3, fontSize: 2 }}>
       <Content />
