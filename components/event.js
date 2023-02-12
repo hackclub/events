4ac2b7229eea491d2e1aab7841a6c2284be31ffc
SerenityUX <95105859+SerenityUX@users.noreply.github.com>
2023-02-12 16:02:59 -0500

    updated ui

--- components/event.js
+++ components/event.js
@@ -10,24 +10,25 @@ const now = (start, end) =>
 const Event = ({ id, slug, title, desc, leader, avatar, start, end, cal }) => (
   <Link href="/[slug]" as={`/${slug}`} passHref>
     <Box
-      as="a"
       sx={{
         position: 'relative',
         textDecoration: 'none',
         bg: 'elevated',
         color: 'text',
-        p: [3, 3]
+        cursor: 'pointer',
+        p: 0,
+        borderRadius: 'extra',
+        boxShadow: 'card',
+        overflow: 'hidden'
       }}
     >
       <Box
         sx={{
           bg: past(end) ? 'sunken' : 'primary',
           color: past(end) ? 'text' : 'white',
-          lineHeight: ['subheading', 'body'],
-          m: -3,
-          py: 2,
           px: 3,
-          mb: 3,
+          py: 3,
+          lineHeight: ['subheading', 'body'],
           strong: { display: ['block', 'inline'] }
         }}
       >
@@ -37,39 +38,41 @@ const Event = ({ id, slug, title, desc, leader, avatar, start, end, cal }) => (
           {tt('{h}:{mm} {a}').render(new Date(end))}
         </Text>
       </Box>
-      <Heading variant="subheadline" sx={{ mt: 0, mb: 1 }}>
-        {title}
-      </Heading>
-      <Flex
-        sx={{
-          alignItems: 'center',
-          color: 'muted'
-        }}
-      >
-        {now(start, end)}
-        {!avatar.includes('emoji') && (
-          <Avatar
-            src={avatar}
-            alt={`${leader} profile picture`}
-            size={24}
-            sx={{ height: 24, mr: 2 }}
+      <Box sx={{ p: 3 }}>
+        <Heading variant="subheadline" sx={{ mt: 0, mb: 1 }}>
+          {title}
+        </Heading>
+        <Flex
+          sx={{
+            alignItems: 'center',
+            color: 'muted'
+          }}
+        >
+          {now(start, end)}
+          {!avatar.includes('emoji') && (
+            <Avatar
+              src={avatar}
+              alt={`${leader} profile picture`}
+              size={24}
+              sx={{ height: 24, mr: 2 }}
+            />
+          )}
+          <Text as="span">{leader}</Text>
+        </Flex>
+        {now(start, end) && (
+          <Sparkles
+            aria-hidden
+            style={{
+              pointerEvents: 'none',
+              position: 'absolute !important',
+              top: 0,
+              left: 0,
+              right: 0,
+              bottom: 0
+            }}
           />
         )}
-        <Text as="span">{leader}</Text>
-      </Flex>
-      {now(start, end) && (
-        <Sparkles
-          aria-hidden
-          style={{
-            pointerEvents: 'none',
-            position: 'absolute !important',
-            top: 0,
-            left: 0,
-            right: 0,
-            bottom: 0
-          }}
-        />
-      )}
+      </Box>
     </Box>
   </Link>
 )
--- pages/index.js
+++ pages/index.js
@@ -1,67 +1,109 @@
 import { Container, Box, Text, Heading, Button, Link as A } from 'theme-ui'
 import Link from 'next/link'
 import Month from '../components/month'
-import { Activity, SkipBack } from 'react-feather'
+import { Activity, SkipBack, XCircle } from 'react-feather'
+import { useState } from 'react'
+export default ({ pastMonths, futureMonths }) => {
+  const [allMonthsViewable, setAllMonthsViewable] = useState(false)
 
-export default ({ months }) => (
-  <>
-    <Box
-      as="header"
-      sx={{
-        bg: 'sheet',
-        textAlign: 'center',
-        px: 3,
-        pb: [3, 4],
-        mb: [3, 4]
-      }}
-    >
-      <Heading as="h1" variant="title" color="primary" mb={2}>
-        Hack Club Events
-      </Heading>
-      <Text as="p" variant="subtitle">
-        AMAs, show & tells, & weekly fun in the{' '}
-        <A href="https://hackclub.com/">Hack Club</A> community.
-      </Text>
-      <Text as="p" variant="subtitle" mt={2}>
-        All dates/times in your local time.
-      </Text>
-    </Box>
-    <Container as="main" px={0}>
-      {Object.keys(months).map(key => (
-        <Month key={key} month={key} events={months[key]} />
-      ))}
-      {Object.keys(months).length== 0 && (
-        <Box sx={{ textAlign: 'center' }}>
-          <h1 sx={{ fontWeight: '400' }}>🚧 More events coming soon.</h1>
-        </Box>
-      )}
+  return (
+    <>
       <Box
-        as="footer"
+        as="header"
         sx={{
-          textAlign: 'center',
-          pb: [4, 5],
-          a: { variant: 'buttons.outline', color: 'secondary', mx: 2 }
+          bg: 'sheet',
+          textAlign: ['left', 'left', 'center'],
+          px: 3,
+          pb: [3, 4],
+          mb: [3, 4]
         }}
       >
-        <Link href="/past" passHref>
-          <Button as="a" variant="outline">
-            <SkipBack />
-            View past events
-          </Button>
-        </Link>
+        <Container>
+          <Heading as="h1" variant="title" color="primary" mb={2}>
+            Hack Club Events
+          </Heading>
+          <Text as="p" variant="subtitle">
+            AMAs, show & tells, & weekly fun in the{' '}
+            <A href="https://hackclub.com/">Hack Club</A> community.
+          </Text>
+          <Text as="p" variant="subtitle" mt={2}>
+            All dates/times in your local time.
+          </Text>
+        </Container>
+      </Box>
+
+      <Container
+        as="main"
+        sx={{
+          textAlign: 'left',
+          pb: [3, 4]
+        }}
+        px={0}
+      >
+        {Object.keys(futureMonths).map(key => (
+          <Month key={key} month={key} events={futureMonths[key]} />
+        ))}
+      </Container>
+      {allMonthsViewable ? (
+        <Container
+          as="main"
+          sx={{
+            textAlign: 'left',
+            pb: [3, 4]
+          }}
+          px={0}
+        >
+          {Object.keys(pastMonths).map(key => (
+            <Month key={key} month={key} events={pastMonths[key]} />
+          ))}
+        </Container>
+      ) : null}
+      <Container>
+        <Button
+          as="a"
+          onClick={() => setAllMonthsViewable(!allMonthsViewable)}
+          variant="outline"
+        >
+          {!allMonthsViewable ? <SkipBack /> : <XCircle />}
+          {!allMonthsViewable ? 'View Past Months' : 'Hide Past Months'}
+        </Button>
         <Link href="/data" passHref>
           <Button as="a">
             <Activity />
             Events API
           </Button>
         </Link>
-      </Box>
-    </Container>
-  </>
-)
+        <Box
+          as="footer"
+          sx={{
+            textAlign: 'center',
+            pb: [4, 5],
+            a: { variant: 'buttons.outline', color: 'secondary', mx: 2 }
+          }}
+        ></Box>{' '}
+      </Container>
+    </>
+  )
+}
 
 export const getStaticProps = async () => {
-  const { getUpcomingMonthly } = require('./api/events/upcoming-monthly')
-  const months = await getUpcomingMonthly()
-  return { props: { months }, revalidate: 1 }
+  const { getEvents } = require('../lib/data')
+  const { groupBy, filter } = require('lodash')
+  let events = await getEvents()
+  let pastEvents = filter(
+    events,
+    e =>
+      new Date(new Date(e.end.substring(0, 7)).toISOString().substring(0, 7)) <
+      new Date(new Date().toISOString().substring(0, 7))
+  )
+  let futureEvents = filter(
+    events,
+    e =>
+      new Date(new Date(e.end.substring(0, 7)).toISOString().substring(0, 7)) >=
+      new Date(new Date().toISOString().substring(0, 7))
+  )
+  const pastMonths = groupBy(pastEvents, e => e.start.substring(0, 7))
+
+  const futureMonths = groupBy(futureEvents, e => e.start.substring(0, 7))
+  return { props: { pastMonths, futureMonths }, revalidate: 5 }
 }
