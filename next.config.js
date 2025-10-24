88ef4547f1a03345cedf1f8b63ad990017ca0d24
Vic <vicxd@pm.me>
2025-10-24 19:30:13 +0200

    Update API documentation and fix CORS headers configuration

--- next.config.js
+++ next.config.js
@@ -1,5 +1,18 @@
 const withMDX = require('@next/mdx')({ extension: /\.mdx?$/ })
 module.exports = withMDX({
   trailingSlash: true,
-  pageExtensions: ['js', 'jsx', 'mdx']
+  pageExtensions: ['js', 'jsx', 'mdx'],
+  async headers(){
+    return [
+      {
+        source: '/api/events/:path*',
+        headers: [
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: '*'
+          }
+        ]
+      }
+    ]
+  }
 })
